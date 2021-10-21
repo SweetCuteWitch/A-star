042f3d4db862d9907505c7e4640d7ee5ba3577ba
SweetCuteWitch <81059271+SweetCuteWitch@users.noreply.github.com>
2021-10-21 13:17:15 +0300

    Update app.js

--- app.js
+++ app.js
@@ -10,11 +10,7 @@ let matrix = [];
 let startPoint = [];
 let endPoint = [];
 let obstacles = [];
-
-const turnPoint = (point) => [point[1], point[0]]
-
-const transponate = (array) => 
-  array[0].map((_, colIndex) => array.map(row => row[colIndex]));
+let diagon = true;
 
 const render = () => {
   let rows = '';
@@ -40,6 +36,23 @@ const render = () => {
       }
   aside.style.display = 'flex'
 }
+const rerender = () => {
+  const arr = document.getElementsByClassName('box');
+  let count = 0;
+  for (let j = 0; j < matrix.length; j++) {
+    for (let i = 0; i < matrix.length; i++) {
+      switch (matrix[j][i].flag) {
+        case'-': arr[count].classList = 'box empty'; break;
+        case's': arr[count].classList = 'box start'; break;
+        case'e': arr[count].classList = 'box end'; break;
+        case'w': arr[count].classList = 'box walked'; break;
+        case'o': arr[count].classList = 'box obs'; break;
+        case'c': arr[count].classList = 'box path'; break;
+      }
+      count ++;
+    }
+  }
+} 
 
 submit.addEventListener('click', () => {
   matrix = input([+xInput.value, +yInput.value]);
@@ -47,54 +60,106 @@ submit.addEventListener('click', () => {
   popup.classList.toggle('disable');
 })
 
+let st = null;
 aside.addEventListener('click', (el) => {
-  const boxes = document.getElementsByClassName('box');
-  let st = null;
-  if (el.target.id == 'start') {
+  if (el.target.id === 'start') {
     el.target.setAttribute('disabled', true);
     st = 0;
   }
-  if (el.target.id == 'end') {
+  if (el.target.id === 'end') {
     el.target.setAttribute('disabled', true);
     st = 1;
   }
-  if (el.target.id == 'obst') {
+  if (el.target.id === 'obst') {
     st = 2;
   }
-  if (el.target.id == 'reset') {
+  if (el.target.id === 'reset') {
     st = 3;
   }
-  if (el.target.id == 'run') {
+  if (el.target.id === 'run') {
     findingCycle(startPoint, endPoint);
     res.reverse().map(el => {
         matrix[el[1]][el[0]] = {flag: 'c'}
-        render();
       })
+    rerender();
   }
-  field.addEventListener('click', (e) => {
+  if (el.target.id === 'reset-field') {
+    matrix.forEach(el => {
+      el.forEach(e => {
+        e.flag = '-';
+      })
+    });
+    buffer = [];
+    trace = [];
+    res = [];
+    obstacles = [];
+    startPoint = [];
+    endPoint = [];
+    mousedown = false;
+    document.getElementById('start').removeAttribute('disabled');
+    document.getElementById('end').removeAttribute('disabled');
+    rerender();
+  }
+  if (el.target.id === 'walk') {
+    el.target.classList.toggle('no');
+    diagon = !diagon
+  }
+  if (el.target.id === 'reset-path') {
+    buffer = [];
+    trace = [];
+    res = [];
+    matrix = [];
+    matrix = input([+xInput.value, +yInput.value]);
+    matrix[startPoint[1]][startPoint[0]] = {flag: 's'};
+    matrix[endPoint[1]][endPoint[0]] = {flag: 'e'};
+    for (let i = 0; i < obstacles.length; i++) {
+      matrix[obstacles[i][1]][obstacles[i][0]] = {flag: 'o'};
+    }
+    rerender();
+  }
+})
+field.addEventListener('click', (e) => {
+  if (e.target.id !== 'field') {
     if (st === 0) {
       st = null
       startPoint = [e.target.innerHTML.split(' ')[1], e.target.innerHTML.split(' ')[0]];
       matrix[startPoint[1]][startPoint[0]] = {flag: 's'};
-      render()
+      e.target.classList.value = 'box start';
     }
     if (st === 1) {
       st = null;
       endPoint = [e.target.innerHTML.split(' ')[1], e.target.innerHTML.split(' ')[0]];
       matrix[endPoint[1]][endPoint[0]] = {flag: 'e'};
-      render()
+      e.target.classList.value = 'box end';
     }
     if (st === 2) {
       obstacles.push([e.target.innerHTML.split(' ')[1], e.target.innerHTML.split(' ')[0]])
       for (let i = 0; i < obstacles.length; i++) {
         matrix[obstacles[i][1]][obstacles[i][0]] = {flag: 'o'};
       }
-      render()
+      e.target.classList.value = 'box obs';
     }
     if (st === 3) {
       point = [e.target.innerHTML.split(' ')[1], e.target.innerHTML.split(' ')[0]];
       matrix[point[1]][point[0]] = {flag: '-'};
-      render()
+      let index = obstacles.indexOf(point);
+      obstacles.splice(index, 1);
+      e.target.classList.value = 'box empty';
     }
-  })
+    mousedown = !mousedown;
+  }
+})
+field.addEventListener('mouseover', (e) => {
+  if (e.target.id !== 'field') {
+    if (mousedown && st === 2) {
+      obstacles.push([e.target.innerHTML.split(' ')[1], e.target.innerHTML.split(' ')[0]])
+      for (let i = 0; i < obstacles.length; i++) {
+        matrix[obstacles[i][1]][obstacles[i][0]] = {flag: 'o'};
+      }
+      e.target.classList.value = 'box obs';
+    }
+  }
+})
+field.addEventListener('mouseleave', () => {
+  mousedown = false;
 })
